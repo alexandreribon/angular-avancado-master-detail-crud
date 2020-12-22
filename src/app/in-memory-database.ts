f1db7299175b52d6a6aa6165cbc3f684630e1561
Alexandre <xribon@gmail.com>
2020-12-22 11:10:27 -0300

    Ativando o Interceptador Http do InMemoryWebApi

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -3,7 +3,7 @@ import { InMemoryDbService } from "angular-in-memory-web-api";
 import { Category } from "./pages/categories/shared/category.model";
 
 
-export class InmemoryDatabase implements InMemoryDbService {
+export class InMemoryDatabase implements InMemoryDbService {
     createDb() {
         const categories: Category[] = [
             { id: 1, name: 'Moradia', description: 'Pagamentos de Contas de Casa' },
