15cfa6522cb44e76bfb20918c2224c34e7323554
Alexandre <xribon@gmail.com>
2021-01-01 14:03:28 -0300

    Concluíndo o formulário de lançamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -7,7 +8,7 @@ import { AppComponent } from './app.component';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDatabase } from './in-memory-database'
-import { from } from 'rxjs';
+
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { from } from 'rxjs';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -6,6 +6,10 @@ import { EntriesRoutingModule } from './entries-routing.module';
 import { EntryListComponent } from './entry-list/entry-list.component';
 import { EntryFormComponent } from './entry-form/entry-form.component';
 
+import { CalendarModule } from 'primeng/calendar';
+import { IMaskModule } from 'angular-imask';
+
+
 @NgModule({
   declarations: [
     EntryListComponent,
@@ -14,7 +18,9 @@ import { EntryFormComponent } from './entry-form/entry-form.component';
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    EntriesRoutingModule
+    EntriesRoutingModule,
+    CalendarModule,
+    IMaskModule
   ]
 })
 export class EntriesModule { }
